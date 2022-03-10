9e452ce6106320548cf6ab3b350c3c0d7c4b1748
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-10 17:24:17 +0100

    Verifico i km inseriti con messaggio di errore

--- main.js
+++ main.js
@@ -20,49 +20,59 @@ genera.addEventListener('click',
         let priceTicket;
         let tipoBiglietto;
 
-        if (eta == 1) {
+        if (km > 0) {
 
-            // Prezzo ticket con 20% di sconto
-            priceKm -= (priceKm * 0.2);
-            priceTicket = priceKm * km;
-            // Tipo di biglietto
-            tipoBiglietto = "Biglietto 20% di sconto";
+            if (eta == 1) {
 
-        } else if (eta == 2) {
+                // Prezzo ticket con 20% di sconto
+                priceKm -= (priceKm * 0.2);
+                priceTicket = priceKm * km;
+                // Tipo di biglietto
+                tipoBiglietto = "Biglietto 20% di sconto";
 
-            // Prezzo standanrd
-            priceTicket = priceKm * km;
-            // Tipo di biglietto
-            tipoBiglietto = "Biglietto standard";
+            } else if (eta == 2) {
+
+                // Prezzo standanrd
+                priceTicket = priceKm * km;
+                // Tipo di biglietto
+                tipoBiglietto = "Biglietto standard";
+            }
+            if (eta == 3) {
+
+                // Prezzo con 40% di sconto
+                priceKm -= (priceKm * 0.4);
+                priceTicket = priceKm * km;
+                // Tipo di biglietto
+                tipoBiglietto = "Biglietto 40% di sconto";
+                console.log("Over 65");
+
+            }
+
+            // Faccio comparire il ticket al click
+            document.getElementById("blockTicket").classList.remove("d-none");
+
+
+            // Mando in stampa i vari dati
+            document.getElementById("nomeUtente").innerHTML = nome;
+            document.getElementById("tipoTicket").innerHTML = tipoBiglietto;
+            document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
+            document.getElementById("codiceCP").innerHTML = Math.floor(Math.random() * 5000) + 4999;
+            document.getElementById("prezzo").innerHTML = priceTicket.toFixed(2) + "&#8364;";
+
+        }else{
+            alert("I km deveno essere maggiori di zero");
+            location.reload();
         }
-        if (eta == 3) {
-
-            // Prezzo con 40% di sconto
-            priceKm -= (priceKm * 0.4);
-            priceTicket = priceKm * km;
-            // Tipo di biglietto
-            tipoBiglietto = "Biglietto 40% di sconto";
-            console.log("Over 65");
-
-        }
-
-        // Faccio comparire il ticket al click
-        document.getElementById("blockTicket").classList.remove("d-none");
 
 
-        // Mando in stampa i vari dati
-        document.getElementById("nomeUtente").innerHTML = nome;
-        document.getElementById("tipoTicket").innerHTML = tipoBiglietto;
-        document.getElementById("carrozza").innerHTML = Math.floor(Math.random()*10) + 1;
-        document.getElementById("codiceCP").innerHTML = Math.floor(Math.random()*5000) + 4999;
-        document.getElementById("prezzo").innerHTML = priceTicket.toFixed(2) + "&#8364;";
 
     }
 )
 
 // Cancello il ticket
 annulla.addEventListener('click',
-    function(){
+    function () {
         document.getElementById("blockTicket").classList.add("d-none");
+        location.reload();
     }
 )
